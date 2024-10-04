498309ed198a0bddf751b31774c3cf903cd0d2e7
Fabio Camargo <fabiorcamargo@gmail.com>
2024-10-04 16:00:33 -0300

    up

--- app/Observers/SpaceObserver.php
+++ app/Observers/SpaceObserver.php
@@ -3,8 +3,10 @@
 namespace App\Observers;
 
 use App\Jobs\DeleteUploadFile;
+use App\Jobs\SendMessage;
 use App\Jobs\UploadFileToSpace;
 use App\Models\File;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 
 class SpaceObserver
@@ -15,25 +17,25 @@ class SpaceObserver
     public function created(File $file): void
     {
 
-    $file->update(['status' => 'subindo']);
+        $file->update(['status' => 'subindo']);
         // Obter o caminho do arquivo armazenado
-    $filePath = $file->file_path; // Caminho relativo do banco de dados, ex: 'uploads/nome_do_arquivo.ext'
-    
-    // Obter o caminho absoluto do arquivo local
-    $fullFilePath = storage_path('app/public/' . $filePath);
-    
-    //dd($fullFilePath, basename($filePath), $file->id);
-    // Verifique se o arquivo existe antes de despachar o job
-    if (file_exists($fullFilePath)) {
-        // Despacha o job para fazer o upload para o DigitalOcean Spaces
-        UploadFileToSpace::dispatch($fullFilePath, basename($filePath), $file->id);
-        
-        // (Opcional) Para deletar o arquivo local após um certo tempo
-        // DeleteUploadFile::dispatch($fullFilePath)->delay(now()->addMinutes(1));
-       
-    }
+        $filePath = $file->file_path; // Caminho relativo do banco de dados, ex: 'uploads/nome_do_arquivo.ext'
 
-       
+        // Obter o caminho absoluto do arquivo local
+        $fullFilePath = storage_path('app/public/' . $filePath);
+
+        //dd($fullFilePath, basename($filePath), $file->id);
+        // Verifique se o arquivo existe antes de despachar o job
+        if (file_exists($fullFilePath)) {
+            // Despacha o job para fazer o upload para o DigitalOcean Spaces
+            UploadFileToSpace::dispatch($fullFilePath, basename($filePath), $file->id);
+
+            // (Opcional) Para deletar o arquivo local após um certo tempo
+            // DeleteUploadFile::dispatch($fullFilePath)->delay(now()->addMinutes(1));
+
+        }
+
+
         // Obter o caminho absoluto do arquivo local
         //$fullFilePath = storage_path('app/public/' . $file->file_path);
 
@@ -54,7 +56,14 @@ class SpaceObserver
     public function deleted(File $file): void
     {
         // Exclui o arquivo do Digital Ocean Spaces
-        Storage::disk('spaces')->delete('uploads/'.$file->id);
+        Storage::disk('spaces')->delete('uploads/' . $file->id);
+
+        $users = User::all(['phone', 'name']);
+
+        foreach ($users as $user) {
+
+            SendMessage::dispatch('55' . $user->phone, 'Olá ' . $user->name . PHP_EOL . 'Arquivo excluído: ' . $file->name)->delay(5);
+        }
     }
 
     /**
